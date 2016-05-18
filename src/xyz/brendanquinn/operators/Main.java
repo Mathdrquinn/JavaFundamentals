5b24e9f76dcd5841e9a18613039acbeb69f5fc93
Brendan Quinn <brendan.quinn@nextgearcapital.com>
2016-05-18 11:26:16 -0400

    build main

--- src/xyz/brendanquinn/operators/Main.java
+++ src/xyz/brendanquinn/operators/Main.java
@@ -7,9 +7,29 @@ public class Main {
 
     public static void main(String[] args) {
         Flight plane1 = new Flight(123, 49);
+        Flight plane2 = new Flight(29, 112);
 
+        System.out.println('Before:');
         System.out.println(plane1.getFlightNumber());
         System.out.println(plane1.getOpenSeats());
+        System.out.println(plane2.getFlightNumber());
+        System.out.println(plane2.getOpenSeats());
+
+        this.swapFlightNumbers(plane1, plane2);
+
+        System.out.println('After:');
+        System.out.println(plane1.getFlightNumber());
+        System.out.println(plane1.getOpenSeats());
+        System.out.println(plane2.getFlightNumber());
+        System.out.println(plane2.getOpenSeats());
+    }
+
+    public void swapFlightNumbers(Flight flight1, Flight flight2) {
+        int flight1Number = flight1.getFlightNumber();
+        int flight2Number = flight2.getFlightNumber();
+
+        flight1.setFlightNumber(flight2Number);
+        flight2.setFlightNumber(flight1Number);
     }
 
 }
